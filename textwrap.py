4a6a9217e221b29daf76a48871dc90c665b3626c
duc.tdm <truongdinhminhduc@gmail.com>
2020-05-09 19:37:08 +0700

    update code for textwrap file

--- textwrap.py
+++ textwrap.py
@@ -1,4 +1,8 @@
-import textwrap
+# Cannot import text wrap anymore, textwrap3 is a downport of textwrap
+from textwrap3 import fill, wrap
+
 
 a_string = 'This is a very very very very long sentence.'
-print(textwrap.fill(a_string, 4))
+print(wrap(a_string, 4))
+
+print(fill(a_string, 8))
